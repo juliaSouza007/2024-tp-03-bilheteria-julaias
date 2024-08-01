01c08716a53415cf6185636ba2eb0058150ad85a
Júlia Martins Souza <juliamss07@gmail.com>
2024-07-31 23:23:56 -0300

    Update Main.java

--- src./Main.java
+++ src./Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,13 +7,17 @@ public class Main {
         
         Evento moana = new Filme("Moana", "08/07/2024", "1:30", "Cinema", 10.0);
         Evento avengers = new Filme("Avengers", "09/07/2024", "14:00", "Cinema", 18.0);
-        Evento filarmonica = new Concerto("Filarmônica", "10/07/2024", "20:00", "Teatro", 50.0);
+        Evento filarmonica = new Concerto("Filarmônica", "10/07/2024", "20:00", "Auditório", 50.0);
+        Evento hamlet = new Teatro("Hamlet", "11/07/2024", "18:00", "Teatro", 100);
+        Evento peterPan = new Teatro("Peter Pan", "12/07/2024", "16:00", "Teatro", 30);
         
-        //adcionar evntos na lista 
+        //adcionar evntos na lista - NÃO ESQUECER DE ADCIONAR EVENTO NA LISTA!!
         eventos.add(moana);
         eventos.add(avengers);
         eventos.add(filarmonica);
-        
+        eventos.add(hamlet);
+        eventos.add(peterPan);
+
         moana.addIngresso(new Ingresso("08/07/2024", moana.getPreco()));
         moana.addIngresso(new IngressoVIP("08/07/2024", moana.getPreco()));
         moana.addIngresso(new Ingresso("08/07/2024", moana.getPreco()));
@@ -46,5 +51,56 @@ public class Main {
         System.out.println("\nReceita Total Acumulada de Todos os Eventos: R$ " + receitaTotalAcumulada);
         
         moana.ingressosVendidos();
+
+        //dialogos para adquirir um ingresso com jOptionPane
+        String[] nomesEventos = {moana.getNome(), avengers.getNome(), filarmonica.getNome(), hamlet.getNome(), peterPan.getNome()};
+
+        String nomeEvento = (String) JOptionPane.showInputDialog(null,
+                "<< COMPRA DE INGRESSO >>",
+                "Seleção de Evento",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                nomesEventos,
+                nomesEventos[0]);
+
+        //encontra o evento selecionado
+        Evento eventoSelecionado = null;
+        for (Evento evento : eventos) {
+            if (evento.getNome().equals(nomeEvento)) {
+                eventoSelecionado = evento;
+                break;
+            }
+        }
+
+        if (eventoSelecionado != null) {
+            //tipos de ingresso
+            Object[] opcoesIngressos = {"Normal", "VIP", "Meia Entrada"};
+
+            String tipoIngresso = (String) JOptionPane.showInputDialog(null,
+                    "<< TIPO DO INGRESSO>>",
+                    "Seleção de Ingresso",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    opcoesIngressos,
+                    "Normal");
+
+            //adiciona o ingresso selecionado ao evento
+            Ingresso ingresso = null;
+            switch (tipoIngresso) {
+                case "Normal":
+                    eventoSelecionado.addIngresso(new Ingresso("08/07/2024", eventoSelecionado.getPreco()));
+                    break;
+                case "VIP":
+                    eventoSelecionado.addIngresso(new IngressoVIP("08/07/2024", eventoSelecionado.getPreco()));
+                    break;
+                case "Meia Entrada":
+                    eventoSelecionado.addIngresso(new IngressoMeiaEntrada("08/07/2024", eventoSelecionado.getPreco()));
+                    break;
+            }
+        }
+
+        //teste de funcionalidade - tudo certo!!
+        receitaTotalAcumulada = Evento.receitaTotalAcumulada(eventos);
+        System.out.println("\nReceita Total Acumulada de Todos os Eventos: R$ " + receitaTotalAcumulada);
     }
 }
