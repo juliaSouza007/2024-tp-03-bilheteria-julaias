73c446537d7604d3a1ad86551a6cd965db4883b4
Júlia Martins Souza <juliamss07@gmail.com>
2024-07-31 22:33:14 -0300

    Delete src directory

--- src/IReceita.java
+++
@@ -1,4 +0,0 @@
-public interface IReceita {
-    public double totalReceita();
-    public void extratoReceita();
-}
--- src/IngressoVIP.java
+++
@@ -1,6 +0,0 @@
-public class IngressoVIP extends Ingresso{
-    public IngressoVIP(String data, double valor) {
-        super(data, valor*2);
-        this.tipo = 'v';
-    }
-}
