2e5edcb808a3a9d04680cdc59642b1e0fb92c577
Júlia Martins Souza <juliamss07@gmail.com>
2024-08-10 13:45:51 -0300

    Update Ingresso.java

--- src./Ingresso.java
+++ src./Ingresso.java
@@ -1,27 +1,20 @@
 import java.util.Date;
-import java.text.SimpleDateFormat;
-import javax.swing.*;
 
 public class Ingresso implements IReceita {
     private Date data;
     protected double valor;
     protected char tipo;
-    public SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy"); // Para converter String para Date
 
-    public Ingresso(String data, double valor) {
-        try{ // Converte String para Date
-            this.data = formatoData.parse(data);
-        }catch (Exception e){
-            System.out.println("<< Date Error >>");
-        }
+    public Ingresso(double valor) {
+        this.data = new Date();
         this.valor = valor;
         this.tipo = 'n';
     }
-    
+
     public double getValor () {
         return this.valor;
     }
-    
+
     public char getTipo() {
         return tipo;
     }
@@ -30,11 +23,11 @@ public class Ingresso implements IReceita {
     public double totalReceita() {
         return valor;       //receita de um ingresso é seu valor
     }
-    
+
     @Override
     public String toString() {
         return "Ingresso [ Data: " + data + " || Valor: " + valor + " || Tipo: " + tipo + " ]";
-    } 
+    }
 
     @Override
     public void extratoReceita() {
